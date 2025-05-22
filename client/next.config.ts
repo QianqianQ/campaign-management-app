17a4f9094d24193456f0f4d86e0e008be22a1f08
Qianqian Qin <qianqian.qin@outlook.com>
2025-05-22 20:56:21 +0300

    Update azure-static-web-apps workflow

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
 };
 
 export default nextConfig;
